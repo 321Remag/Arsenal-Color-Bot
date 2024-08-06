d619566dbdc09176c9e85f708dc46b78107e0e8c
Extovia Team <jqgsmfang@gmail.com>
2024-08-06 20:53:03 +0200

    Add files via upload

---
+++ Extovia.py
@@ -0,0 +1,27 @@
+from colorama import Fore
+from colorama import init as colorama_init
+import os
+import time
+import random
+
+os.system('cls')
+
+time.sleep(2)
+os.system('cls')
+print(Fore.MAGENTA + """▓█████ ▒██   ██▒▄▄▄█████▓ ▒█████   ██▒   █▓ ██▓ ▄▄▄      
+    ▓█   ▀ ▒▒ █ █ ▒░▓  ██▒ ▓▒▒██▒  ██▒▓██░   █▒▓██▒▒████▄    
+    ▒███   ░░  █   ░▒ ▓██░ ▒░▒██░  ██▒ ▓██  █▒░▒██▒▒██  ▀█▄  
+    ▒▓█  ▄  ░ █ █ ▒ ░ ▓██▓ ░ ▒██   ██░  ▒██ █░░░██░░██▄▄▄▄██ 
+    ░▒████▒▒██▒ ▒██▒  ▒██▒ ░ ░ ████▓▒░   ▒▀█░  ░██░ ▓█   ▓██▒
+    ░░ ▒░ ░▒▒ ░ ░▓ ░  ▒ ░░   ░ ▒░▒░▒░    ░ ▐░  ░▓   ▒▒   ▓▒█░
+     ░ ░  ░░░   ░▒ ░    ░      ░ ▒ ▒░    ░ ░░   ▒ ░  ▒   ▒▒ ░
+       ░    ░    ░    ░      ░ ░ ░ ▒       ░░   ▒ ░  ░   ▒   
+       ░  ░ ░    ░               ░ ░        ░   ░        ░  ░
+                                           ░                 """)
+print("")
+print("    Checking for Updates")
+
+time.sleep(15)
+os.system('cls')
+
+os.startfile('Extovia_Launcher.exe')
